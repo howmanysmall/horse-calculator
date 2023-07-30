2d4b39c2640432fb50439b4eab5c4d17b6bbbb09
howmanysmall <lhamil29@bruinmail.slcc.edu>
2023-07-30 10:46:42 -0600

    anew

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,33 +2,17 @@ import FormRow from "./form-row";
 import type { RecoilState } from "recoil";
 
 export interface IFormProperties {
-  rowData: Array<
-    [
-      id: number,
-      summaryState: [percentage: number, isSafe: boolean],
-      valueState: RecoilState<number>
-    ]
-  >;
+	rowData: Array<[id: number, summaryState: [percentage: number, isSafe: boolean], valueState: RecoilState<number>]>;
 }
 
 export const Form: React.FC<IFormProperties> = ({ rowData }) => {
-  return (
-    <div role="list">
-      {rowData.map(([id, summaryState, valueState]) => (
-        <FormRow
-          key={`Row${id}`}
-          id={id}
-          summaryState={summaryState}
-          valueState={valueState}
-        />
-      ))}
-
-      {/* <br />
-			<button type="button" className="bg-green-500 text-white text-lg py-2 px-4 rounded" id="calculate">
-				Calculate
-			</button> */}
-    </div>
-  );
+	return (
+		<div role="list">
+			{rowData.map(([id, summaryState, valueState]) => (
+				<FormRow key={`Row${id}`} id={id} summaryState={summaryState} valueState={valueState} />
+			))}
+		</div>
+	);
 };
 
 export default Form;
